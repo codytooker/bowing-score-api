7ad6ca28384171faf11fa18a72240e526adeb423
Cody <cody@dreamersofday.co>
2018-10-08 19:11:01 -0500

    add user custom claims

--- app/User.php
+++ app/User.php
@@ -45,6 +45,12 @@ class User extends Authenticatable implements JWTSubject
      */
     public function getJWTCustomClaims()
     {
-        return [];
+        return [
+            'user' => [
+                'id' => $this->id,
+                'name' => $this->name,
+                'email' => $this->email,
+            ]
+        ];
     }
 }
