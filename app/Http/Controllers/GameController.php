9b3098b785d775f962648a18c9cfa52ef2557d1b
Cody <cody@dreamersofday.co>
2018-10-15 15:39:41 -0500

    add game controller and post game store route

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -25,7 +25,7 @@ class GameController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        return $request;
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -10,3 +10,7 @@ Route::prefix('auth')->group(function () {
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');
 });
+
+Route::middleware('auth:api')->group(function () {
+    Route::post('game', 'GameController@store');
+});
